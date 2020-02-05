3b5400afd2feb0f7d339df88f82ecb7ebdcf7617
mxtarzan <mxtarzan@gmail.com>
2020-02-05 16:17:32 -0500

    building main class

---
+++ src/base/picture.java
@@ -0,0 +1,12 @@
+package base;
+
+public class picture {
+	public static int day;
+	public static int month;
+	public static int year;
+	public static int hour;
+	public static int minute;
+	public static int second;
+	public static int height;
+	public static int shadowlength;
+}
